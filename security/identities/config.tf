537d78ce8603982c0c77215ae2e0ebfe2200c2c9
Angelo Fenoglio <angelofenoglio@gmail.com>
2021-06-03 17:28:49 -0300

    Add security account identities layer

---
+++ security/identities/config.tf
@@ -0,0 +1,45 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.12.28"
+
+  required_providers {
+    aws = "~> 3.27"
+  }
+
+  backend "s3" {
+    key = "security/identities/terraform.tfstate"
+  }
+}
+ #Ask what about these
+data "terraform_remote_state" "apps-devstg-keys" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-apps-devstg-devops"
+    bucket  = "${var.project}-apps-devstg-terraform-backend"
+    key     = "apps-devstg/security-keys/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "apps-devstg-storage-bucket-demo-files" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-apps-devstg-devops"
+    bucket  = "${var.project}-apps-devstg-terraform-backend"
+    key     = "apps-devstg/storage-bucket-demo-files/terraform.tfstate"
+  }
+}
