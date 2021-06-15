e5f0b8064b5c688c6d4fa752561f5369011c1b57
Angelo Fenoglio <angelofenoglio@gmail.com>
2021-06-15 15:14:28 -0300

    Indent policies

--- management/organizations/policies_scp.tf
+++ management/organizations/policies_scp.tf
@@ -10,34 +10,34 @@ resource "aws_organizations_policy" "default" {
   name        = "default"
   description = "Default SCP: this is a default minimal policy. Eg: security acct."
 
-  content = <<JSON
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": [
-        "aws-portal:*",
-        "cloudtrail:*",
-        "cloudwatch:*",
-        "config:*",
-        "dynamodb:*",
-        "events:*",
-        "guardduty:*",
-        "health:*",
-        "iam:*",
-        "inspector:*",
-        "kms:*",
-        "lambda:*",
-        "logs:*",
-        "s3:*",
-        "ssm:*",
-        "support:*",
-        "tag:*"
-    ],
-    "Resource": "*"
+  content = <<-JSON
+  {
+      "Version": "2012-10-17",
+      "Statement": {
+          "Effect": "Allow",
+          "Action": [
+              "aws-portal:*",
+              "cloudtrail:*",
+              "cloudwatch:*",
+              "config:*",
+              "dynamodb:*",
+              "events:*",
+              "guardduty:*",
+              "health:*",
+              "iam:*",
+              "inspector:*",
+              "kms:*",
+              "lambda:*",
+              "logs:*",
+              "s3:*",
+              "ssm:*",
+              "support:*",
+              "tag:*"
+          ],
+          "Resource": "*"
+      }
   }
-}
-JSON
+  JSON
 }
 
 #
@@ -48,58 +48,58 @@ resource "aws_organizations_policy" "standard" {
   name        = "standard"
   description = "Standard SCP: this policy is the one that is typically used by most accounts. Eg: shared, apps_devstg and apps_prd accts."
 
-  content = <<JSON
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": [
-        "acm:*",
-        "athena:*",
-        "autoscaling:*",
-        "application-autoscaling:*",
-        "aws-portal:*",
-        "backup:*",
-        "backup-storage:*",
-        "ce:*",
-        "cloudformation:*",
-        "cloudfront:*",
-        "cloudtrail:*",
-        "cloudwatch:*",
-        "config:*",
-        "dynamodb:*",
-        "ec2:*",
-        "ecr:*",
-        "ecs:*",
-        "elasticloadbalancing:*",
-        "events:*",
-        "guardduty:*",
-        "glacier:*",
-        "glue:*",
-        "health:*",
-        "iam:*",
-        "inspector:*",
-        "kms:*",
-        "lambda:*",
-        "logs:*",
-        "redshift:*",
-        "rds:*",
-        "route53:*",
-        "route53domains:*",
-        "route53resolver:*",
-        "s3:*",
-        "secretsmanager:*",
-        "shield:*",
-        "sns:*",
-        "sqs:*",
-        "ssm:*",
-        "support:*",
-        "tag:*",
-        "waf:*",
-        "waf-regional:*"
-    ],
-    "Resource": "*"
+  content = <<-JSON
+  {
+      "Version": "2012-10-17",
+      "Statement": {
+          "Effect": "Allow",
+          "Action": [
+              "acm:*",
+              "athena:*",
+              "autoscaling:*",
+              "application-autoscaling:*",
+              "aws-portal:*",
+              "backup:*",
+              "backup-storage:*",
+              "ce:*",
+              "cloudformation:*",
+              "cloudfront:*",
+              "cloudtrail:*",
+              "cloudwatch:*",
+              "config:*",
+              "dynamodb:*",
+              "ec2:*",
+              "ecr:*",
+              "ecs:*",
+              "elasticloadbalancing:*",
+              "events:*",
+              "guardduty:*",
+              "glacier:*",
+              "glue:*",
+              "health:*",
+              "iam:*",
+              "inspector:*",
+              "kms:*",
+              "lambda:*",
+              "logs:*",
+              "redshift:*",
+              "rds:*",
+              "route53:*",
+              "route53domains:*",
+              "route53resolver:*",
+              "s3:*",
+              "secretsmanager:*",
+              "shield:*",
+              "sns:*",
+              "sqs:*",
+              "ssm:*",
+              "support:*",
+              "tag:*",
+              "waf:*",
+              "waf-regional:*"
+          ],
+          "Resource": "*"
+      }
   }
-}
-JSON
+  JSON
 }
