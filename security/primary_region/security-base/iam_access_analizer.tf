ab788fd7b5c60b507067f7f3b3d85ef44077256d
Angelo Fenoglio <angelofenoglio@gmail.com>
2021-11-05 15:26:00 -0300

    Move template into its own directory

--- security/primary_region/security-base/iam_access_analizer.tf
+++
@@ -1,8 +0,0 @@
-#
-# Access Analyzer
-#
-resource "aws_accessanalyzer_analyzer" "default" {
-  analyzer_name = "${var.project}-${var.environment}-access-analyzer"
-  type          = "ORGANIZATION"
-  tags          = local.tags
-}
--- security/primary_region/security-base/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  tags = {
-    Name      = "${var.project}-${var.environment}-cloudtrail-org"
-    Namespace = var.project
-    Stage     = var.environment
-  }
-}
