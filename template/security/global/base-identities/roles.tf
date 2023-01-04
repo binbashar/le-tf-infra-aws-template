f139935bb7c9696e5200eaf158ac0d9b03db031d
Ezequiel Godoy <contacto@ezegodoy.com>
2023-01-04 15:08:11 -0300

    remove role

--- template/security/global/base-identities/roles.tf
+++ template/security/global/base-identities/roles.tf
@@ -29,31 +29,6 @@ module "iam_assumable_role_devops" {
   tags = local.tags
 }
 
-#
-# Assumable Role Cross-Account: Admin
-#
-module "iam_assumable_role_admin" {
-  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-assumable-role?ref=v4.1.0"
-
-  trusted_role_arns = [
-    "arn:aws:iam::${var.security_account_id}:root"
-  ]
-
-  create_role           = true
-  role_name             = "Admin"
-  admin_role_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  attach_admin_policy   = true
-  role_path             = "/"
-
-  #
-  # MFA setup
-  #
-  role_requires_mfa    = true
-  mfa_age              = 43200 # Maximum CLI/API session duration in seconds between 3600 and 43200
-  max_session_duration = 3600  # Max age of valid MFA (in seconds) for roles which require MFA
-
-  tags = local.tags
-}
 
 #
 # Assumable Role Cross-Account: Auditor Role
