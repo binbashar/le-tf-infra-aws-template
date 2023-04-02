368b9b6ec5347d560f64fd0bb59a44b6e1ab8e24
Diego OJ <diego.ojeda@binbash.com.ar>
2023-04-02 15:24:16 -0300

    SSO account assignments needs to be commented by default

--- template/management/global/sso/account_assignments.tf
+++ template/management/global/sso/account_assignments.tf
@@ -8,78 +8,78 @@
 # permission set that uses it. The same applies to when you remove or rename
 # a group (or user) that is referenced by a permission set.
 #
-module "account_assignments" {
-  source = "github.com/binbashar/terraform-aws-sso.git//modules/account-assignments?ref=0.7.1"
+# module "account_assignments" {
+#   source = "github.com/binbashar/terraform-aws-sso.git//modules/account-assignments?ref=0.7.1"
 
-  account_assignments = [
-    # -------------------------------------------------------------------------
-    # Administrators Permissions
-    # -------------------------------------------------------------------------
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["Administrator"].arn
-      permission_set_name = "Administrator"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["administrators"].name
-      account             = var.accounts.management.id
-    },
+#   account_assignments = [
+#     # -------------------------------------------------------------------------
+#     # Administrators Permissions
+#     # -------------------------------------------------------------------------
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["Administrator"].arn
+#       permission_set_name = "Administrator"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["administrators"].name
+#       account             = var.accounts.management.id
+#     },
 
-    # -------------------------------------------------------------------------
-    # DevOps Permissions
-    # -------------------------------------------------------------------------
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["DevOps"].arn
-      permission_set_name = "DevOps"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["devops"].name
-      account             = var.accounts.security.id
-    },
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["DevOps"].arn
-      permission_set_name = "DevOps"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["devops"].name
-      account             = var.accounts.shared.id
-    },
+#     # -------------------------------------------------------------------------
+#     # DevOps Permissions
+#     # -------------------------------------------------------------------------
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["DevOps"].arn
+#       permission_set_name = "DevOps"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["devops"].name
+#       account             = var.accounts.security.id
+#     },
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["DevOps"].arn
+#       permission_set_name = "DevOps"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["devops"].name
+#       account             = var.accounts.shared.id
+#     },
 
-    # -------------------------------------------------------------------------
-    # FinOps Permissions
-    # -------------------------------------------------------------------------
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["FinOps"].arn,
-      permission_set_name = "FinOps",
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["finops"].name
-      account             = var.accounts.management.id,
-    },
+#     # -------------------------------------------------------------------------
+#     # FinOps Permissions
+#     # -------------------------------------------------------------------------
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["FinOps"].arn,
+#       permission_set_name = "FinOps",
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["finops"].name
+#       account             = var.accounts.management.id,
+#     },
 
-    # -------------------------------------------------------------------------
-    # SecurityAuditor Permissions
-    # -------------------------------------------------------------------------
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["SecurityAuditor"].arn
-      permission_set_name = "SecurityAuditor"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["securityauditor"].name
-      account             = var.accounts.security.id
-    },
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["SecurityAuditor"].arn
-      permission_set_name = "SecurityAuditor"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["securityauditor"].name
-      account             = var.accounts.shared.id
-    },
+#     # -------------------------------------------------------------------------
+#     # SecurityAuditor Permissions
+#     # -------------------------------------------------------------------------
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["SecurityAuditor"].arn
+#       permission_set_name = "SecurityAuditor"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["securityauditor"].name
+#       account             = var.accounts.security.id
+#     },
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["SecurityAuditor"].arn
+#       permission_set_name = "SecurityAuditor"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["securityauditor"].name
+#       account             = var.accounts.shared.id
+#     },
 
-    # -------------------------------------------------------------------------
-    # ReadOnly Permissions
-    # -------------------------------------------------------------------------
-    {
-      permission_set_arn  = module.permission_sets.permission_sets["ReadOnly"].arn
-      permission_set_name = "ReadOnly"
-      principal_type      = local.principal_type_group
-      principal_name      = local.groups["readonly"].name
-      account             = var.accounts.shared.id
-    },
+#     # -------------------------------------------------------------------------
+#     # ReadOnly Permissions
+#     # -------------------------------------------------------------------------
+#     {
+#       permission_set_arn  = module.permission_sets.permission_sets["ReadOnly"].arn
+#       permission_set_name = "ReadOnly"
+#       principal_type      = local.principal_type_group
+#       principal_name      = local.groups["readonly"].name
+#       account             = var.accounts.shared.id
+#     },
 
-  ]
-}
+#   ]
+# }
