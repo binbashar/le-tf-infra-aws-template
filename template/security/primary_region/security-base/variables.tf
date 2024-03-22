16698faa57700ba7b76ffb1be47e53690f4864d2
rodriguez-matias <matias.rodriguez@binbash.com.ar>
2024-03-22 17:01:15 -0300

    feat(security-base): update aws/tf versions and add root notification module

--- template/security/primary_region/security-base/variables.tf
+++ template/security/primary_region/security-base/variables.tf
@@ -1,3 +1,13 @@
+#=============================#
+# Notifications               #
+#=============================#
+#
+# AWS SNS -> Lambda -> Slack: tools-monitoring-sec
+#
+variable "sns_topic_name_monitoring_sec" {
+  description = ""
+  default     = "sns-topic-slack-notify-monitoring-sec"
+
 #
 # security/config/backend.tfvars
 #
--- template/shared/primary_region/security-base/variables.tf
+++ template/shared/primary_region/security-base/variables.tf
@@ -1,3 +1,14 @@
+#=============================#
+# Notifications               #
+#=============================#
+#
+# AWS SNS -> Lambda -> Slack: tools-monitoring-sec
+#
+variable "sns_topic_name_monitoring_sec" {
+  description = ""
+  default     = "sns-topic-slack-notify-monitoring-sec"
+}
+
 #
 # shared/config/backend.tfvars
 #
